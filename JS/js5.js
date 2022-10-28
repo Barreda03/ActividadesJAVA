3e442fe8868500b3b24c25636098488322f116e7
Barreda03 <angeles.barredarg@gmail.com>
2022-10-27 20:40:55 -0500

    modif

--- JS/js5.js
+++ JS/js5.js
@@ -1,63 +1,24 @@
-function calculadora(){
-    let num1 = parseInt(document.getElementById("numero-1").value);
-    let num2 = parseInt(document.getElementById("numero-2").value);
-    
-    let signo = document.getElementById("signo").value; 
-    let resultado = document.getElementById("resul");
+function calcular(){
+    let primer_numero = parseInt(document.getElementById("primer_numero").value);
+    let segundo_numero = parseInt(document.getElementById("segundo_numero").value);
+    let operacion = document.getElementById("operacion").value;
 
+    let resultado = 0;
 
-    switch (signo) {
-        case '1':
-            resultado.value = num1 + num2;
-            break;
-
-        case '2':
-            resultado.value = num1 - num2;
-            break;
-
-        case '3':
-            resultado.value = num1 * num2;
-            break;
-            
-        case '4':
-            resultado.value = num1 / num2;
-            break;
-
-        default:
-            break;
+    if (operacion == "+"){
+        resultado = primer_numero + segundo_numero;
     }
-}
+    else if (operacion == "-"){
+        resultado = primer_numero - segundo_numero;
+    }
+    else if (operacion == "*"){
+        resultado = primer_numero * segundo_numero;
+    }
+    else if (operacion == "/"){
+        resultado = primer_numero / segundo_numero;
+    }
 
-function generarTabla() {
-    let numtabla = parseInt(document.getElementById("tabla").value);
-    let tipo = parseInt(document.getElementById("tipo").value);
-    let longitud = parseInt(document.getElementById("long").value);
-    let tabla = document.getElementById("tabla-resul");
-    let push = document.createDocumentFragment();
-
-    document.getElementById("subtitulo").textContent = `Tabla del ${numtabla}`;
-    
-    switch (tipo) {
-        case 1:
-            for (let i = 0; i <= longitud; i++) {
-                let renglon = document.createElement("tr")
-                renglon.innerHTML = `<td>${numtabla}</td><td>x</td><td>${i}</td><td>=</td><td>${(numtabla * i)}</td>`;
-                push.appendChild(renglon);
-            };
-            tabla.innerHTML = "";
-            tabla.appendChild(push);
-            break;
-        case 2:
-            for (let i = longitud; i >= 0; i--) {
-                let renglon = document.createElement("tr")
-                renglon.innerHTML = `<td>${numtabla}</td><td>x</td><td>${i}</td><td>=</td><td>${(numtabla * i)}</td>`;
-                push.appendChild(renglon);
-            };
-            tabla.innerHTML = "";
-            tabla.appendChild(push);
-            break;
-    
-    
-};
+    let resul = document.getElementById("resultado");
+    resul.value = resultado;
 
 }
