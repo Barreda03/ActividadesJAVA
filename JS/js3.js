382ad829fa9f055cae62d2849b66a004f9895213
Barreda03 <angeles.barredarg@gmail.com>
2022-10-28 00:21:00 -0500

    modif2

--- JS/js3.js
+++ JS/js3.js
@@ -1,8 +1,8 @@
 /* Ejemplo Arreglos */
 
-let salario = 0
-let horas = 48
-let pagoxhora = 30
+let salario = 0;
+let horas = 48;
+let pagoxhora = 30;
 
 
 /* Si no tiene horas extras */
@@ -11,7 +11,28 @@ if (horas <= 40) {
     console.log(salario)
 }
 /* Si tiene horas extras */
-else{
+else {
     let salario = (40 * pagoxhora) + ((horas-40) * pagoxhora*3)
     console.log(salario)
+}
+
+/* TOMAS DE MEDICINA */
+
+let tomainicial = 22;
+console.log(`Hora de la toma inicial: ${tomainicial}`)
+
+let tomadiaria = 4;
+console.log(`Frecuencia de la toma diaria: ${tomadiaria}`)
+
+let cantidad_tomas = 10;
+console.log(`Cantidad de tomas: ${cantidad_tomas}`)
+
+let hora = tomainicial;
+
+for (i = 1; i <= cantidad_tomas; i++) {
+    if (hora > 22) {
+        hora -= 21;
+    }
+    console.log("toma" + " " + i + " " + hora + ":00");
+    hora += tomadiaria;
 }
