ca790805e836dbb12717cb602b1328d192322df8
Barreda03 <angeles.barredarg@gmail.com>
2022-10-27 20:36:48 -0500

    js6

---
+++ JS/js6.js
@@ -0,0 +1,11 @@
+function tabla(){
+    let numero_tabla = parseInt(document.getElementById("tabla").value);
+    let longitud = parseInt(document.getElementById("longitud").value);
+    let tipo = document.getElementById("tipo").value;
+
+    for(let i = 1; i <= longitud; i++){
+        document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+    }
+}
